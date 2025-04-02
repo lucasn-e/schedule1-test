ff47f781d944b689e989d112f3ecf6b5b8cee552
Nnay <xxsns2643@gmail.com>
2025-04-02 10:44:24 +0200

    initiainitiall

---
+++ src/attributes.js
@@ -0,0 +1,142 @@
+export default {
+   "Anti-Gravity": {
+      name: "Anti-Gravity",
+      modifier: 0.54
+   },
+   "Athletic": {
+      name: "Athletic",
+      modifier: 0.32
+   },
+   "Balding": {
+      name: "Balding",
+      modifier: 0.3
+   },
+   "Bright-Eyed": {
+      name: "Bright-Eyed",
+      modifier: 0.4
+   },
+   "Calming": {
+      name: "Calming",
+      modifier: 0.1
+   },
+   "Calorie-Dense": {
+      name: "Calorie-Dense",
+      modifier: 0.28
+   },
+   "Cyclopean": {
+      name: "Cyclopean",
+      modifier: 0.56
+   },
+   "Disorienting": {
+      name: "Disorienting",
+      modifier: 0.00
+   },
+   "Electrifying": {
+      name: "Electrifying",
+      modifier: 0.5
+   },
+   "Energizing": {
+      name: "Energizing",
+      modifier: 0.22
+   },
+   "Euphoric": {
+      name: "Euphoric",
+      modifier: 0.18
+   },
+   "Explosive": {
+      name: "Explosive",
+      modifier: 0.00
+   },
+   "Focused": {
+      name: "Focused",
+      modifier: 0.0
+   },
+   "Foggy": {
+      name: "Foggy",
+      modifier: 0.36
+   },
+   "Gingeritis": {
+      name: "Gingeritis",
+      modifier: 0.2
+   },
+   "Glowing": {
+      name: "Glowing",
+      modifier: 0.48
+   },
+   "Jennerising": {
+      name: "Jennerising",
+      modifier: 0.42
+   },
+   "Laxative": {
+      name: "Laxative",
+      modifier: 0.00
+   },
+   "Lethal": {
+      name: "Lethal",
+      modifier: 0.00
+   },
+   "Long Faced": {
+      name: "Long Faced",
+      modifier: 0.52
+   },
+   "Munchies": {
+      name: "Munchies",
+      modifier: 0.0
+   },
+   "Paranoia": {
+      name: "Paranoia",
+      modifier: 0.00
+   },
+   "Refreshing": {
+      name: "Refreshing",
+      modifier: 0.0
+   },
+   "Schizophrenia": {
+      name: "Schizophrenia",
+      modifier: 0.00
+   },
+   "Sedating": {
+      name: "Sedating",
+      modifier: 0.26
+   },
+   "Seizure-Inducing": {
+      name: "Seizure-Inducing",
+      modifier: 0.00
+   },
+   "Shrinking": {
+      name: "Shrinking",
+      modifier: 0.6
+   },
+   "Slippery": {
+      name: "Slippery",
+      modifier: 0.34
+   },
+   "Smelly": {
+      name: "Smelly",
+      modifier: 0.00
+   },
+   "Sneaky": {
+      name: "Sneaky",
+      modifier: 0.24
+   },
+   "Spicy": {
+      name: "Spicy",
+      modifier: 0.38
+   },
+   "Thought-Provoking": {
+      name: "Thought-Provoking",
+      modifier: 0.44
+   },
+   "Toxic": {
+      name: "Toxic",
+      modifier: 0.00
+   },
+   "Tropic Thunder": {
+      name: "Tropic Thunder",
+      modifier: 0.46
+   },
+   "Zombifying": {
+      name: "Zombifying",
+      modifier: 0.58
+   }
+}
---
+++ vue.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+   publicPath: '/schedule1-test/'
+}
